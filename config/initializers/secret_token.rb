f6d023f79d062dc4d3d988fc2c247e043577033f
unrar <catbuntu@catbuntu.me>
2013-03-29 13:35:38 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '01e2df3edcc778f6c4269f75320e66fc2c8d5004d8b268f581c7bc995622c620b0b5aaa9eb91533763298d85a66029081963b830a1781e1f3cb41bf6f68ffc0d'
